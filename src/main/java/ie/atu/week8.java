ede33dfc77307b9b3f713f62ce88684f6dbf9571
chloebeirne <g00399850@atu.ie>
2022-11-15 14:47:54 +0000

    Commit printer code

--- src/main/java/ie/atu/week8.java
+++ src/main/java/ie/atu/week8.java
@@ -1,23 +1,22 @@
 package ie.atu;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.util.Scanner;
 
 public class week8 {
     public static void main(String[] args){
 
+        try
         {
-            File myFile = new File("MyFile.txt");
-            System.out.println("My file is located at " + myFile.getAbsolutePath());
-            String content = "Details to write";
-                try
-                {
-                    FileWriter myWriter = new FileWriter(myFile, true);
-                    myWriter.write("Hello World");
-                    myWriter.close();
-                } catch(IOException e)
-                {
-                    e.printStackTrace();
-                }
+            PrintWriter myWriter = new PrintWriter(new FileWriter("names.txt", true));
+
+            myWriter.println("Add another");
+            myWriter.close();
+        } catch(IOException e)
+        {
+            e.printStackTrace();
         }
+
     }
 }
