0343fde4ed4519daf442af83b56659fdcea2f195
chloebeirne <g00399850@atu.ie>
2022-11-15 14:05:53 +0000

    Commit new code to featureGetDetails

--- src/main/java/ie/atu/week8.java
+++ src/main/java/ie/atu/week8.java
@@ -8,7 +8,16 @@ public class week8 {
         {
             File myFile = new File("MyFile.txt");
             System.out.println("My file is located at " + myFile.getAbsolutePath());
-
+            String content = "Details to write";
+                try
+                {
+                    FileWriter myWriter = new FileWriter(myFile, true);
+                    myWriter.write("Hello World");
+                    myWriter.close();
+                } catch(IOException e)
+                {
+                    e.printStackTrace();
+                }
         }
     }
 }
