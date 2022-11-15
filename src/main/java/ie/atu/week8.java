ea263757df7c049e3ac85f1f520a892c79f71ba5
chloebeirne <g00399850@atu.ie>
2022-11-15 13:25:37 +0000

    Commit new code to master

--- src/main/java/ie/atu/week8.java
+++ src/main/java/ie/atu/week8.java
@@ -1,4 +1,14 @@
 package ie.atu;
 
+import java.io.*;
+
 public class week8 {
+    public static void main(String[] args){
+
+        {
+            File myFile = new File("MyFile.txt");
+            System.out.println("My file is located at " + myFile.getAbsolutePath());
+
+        }
+    }
 }
